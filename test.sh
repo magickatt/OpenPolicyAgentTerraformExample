658a8f8e7d43f30e48379720f51e38006c90b0d2
Andrew Kirkpatrick <andrew.kirkpatrick@stackadapt.com>
2023-03-22 13:35:28 -0400

    Fixing syntax error

--- test.sh
+++ test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Prevent GitHub Action from exiting early
+set +e pipefail
+
+# Check if the Variable representing all applicable Rules is true/false
 opa eval --input plan.json --data policies/ data.enforce_policies.allow | jq '.result[0].expressions[0].value' --raw-output --exit-status &>/dev/null
-if [ "$?" -ne 0 ]; then
+OPA_EVALUATION="$?"
+
+# If the evaluation fails, provide more information to explain why
+if [ "$OPA_EVALUATION" -ne 0 ]; then
     echo "Terraform plan violates provided Open Policy Agent policies."
-    exit $?
+    exit $OPA_EVALUATION
 else
     echo "Terraform plan passes provided Open Policy Agent policies."
 fi
